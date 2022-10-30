5b3d4159e859fa70723bdb20fbf4537aa46acf6b
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-10-30 16:39:30 +0700

    add method post

--- server.js
+++ server.js
@@ -8,28 +8,61 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-app.post("/", async (req, res) => {
-    const destUrl = req.body.destUrl
-    const body = req.body;
-    delete body.dest
-
-  var data = await axios
-    .post(
-        destUrl,body,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-    .then(function (response) {
-      return response;
+app.get("/", async (req, res) => {
+  
+  let data = await axios.get('https://api.ipify.org/?format=json');
+  console.log(data.data.ip);
+  res.send(
+    JSON.stringify({
+      "ip server" : data.data.ip,
+      "version"   : "1.0.0", 
     })
-    .catch(function (error) {
-      return error.response;
-    });
-    
-  res.status(data.status).send(data.data);
+  );
+})
+
+app.post("/", async (req, res) => {
+  const method = req.body.method;
+  const destUrl = req.body.destUrl;
+  if (method.toLowerCase() == "post") {
+    const Authorization = req.body.authorization;
+    const body = req.body;
+    delete body.dest;
+    delete body.Authorization;
+
+    var headersPost;
+    if (Authorization == undefined) {
+      headersPost = {
+        "Content-Type": "application/json",
+      };
+    } else {
+      headersPost = {
+        "Content-Type": "application/json",
+        Authorization: Authorization,
+      };
+    }
+
+    var data = await axios
+      .post(destUrl, body, {
+        headers: headersPost,
+      })
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        return error.response;
+      });
+
+      try{
+        res.status(data.status).send(data.data);
+
+      }catch{
+        res.status(400).send("terjadi kesalahan");
+      }
+
+  } else {
+    let data = await axios.get(destUrl);
+    res.status(data.status).send(data.data);
+  }
 });
 
 const port = process.env.PORT || 3000;
