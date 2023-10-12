0a7e3fdfb16b803b1cf8b8a2f8f8f536b24ae8f6
rifki ahmad sururi <rifkisururi27@gmail.com>
2023-10-12 14:58:18 +0700

    transaksi get

--- server.js
+++ server.js
@@ -65,6 +65,99 @@ app.post("/", async (req, res) => {
   }
 });
 
+
+app.get("/transaksiGet", async (req, res) => {
+  try{
+    console.log(req.body);
+    const destUrl = req.body.destUrl;
+    let data = await axios.get(destUrl);
+    const text =  data;
+    const regex1 = /R#(\d+)\s+([^@]+)\s+akan diproses @(\d+:\d+).+?Saldo ([^\s]+) - ([^\s]+) = ([^\s]+) ([^\d]+)/;
+    const regex2 = /#R([^@]+)\s+([^@]+)\s+@([^@]+), status ([^@]+).+?Sal ([^\s]+)/;
+    const regex3 = /R#([^@]+)\s+([^@]+)\s+SUKSES. SNRef: ([^@]+).+?Saldo ([^@]+) - ([^@]+)=([^@]+)\s/;
+    const regex4 = /R#([^@]+)\s+([^@]+)\s+?GAGAL. . Saldo ([^@]+)+@(\d+:\d+)/;
+  
+    var  match = null
+    var loop = 0;
+    while(match == null){
+      loop++
+      if(loop == 1){
+        match = text.match(regex1);
+      }else if(loop == 2){
+        match = text.match(regex2);
+      }else if(loop == 3){
+        match = text.match(regex3);
+      }else if(loop == 4){
+        match = text.match(regex4);
+      }else{
+        break;
+      }
+    }
+
+    var jsonResult;
+    if (match) {
+      if(loop == 1){
+        jsonResult = {
+          Reference: match[1],
+          Type: match[2],
+          ProcessTime: match[3],
+          StartingBalance: match[4],
+          Amount: match[5],
+          EndingBalance: match[6],
+          TransactionType:  match[7],
+          Status:"Menunggu Jawaban"
+        };
+      }else if(loop == 2){
+        jsonResult = {
+          Reference: match[1],
+          Type: match[2],
+          ProcessTime: match[3],
+          Status: match[4],
+          Amount: match[5]
+        };
+      }else if(loop == 3){
+        jsonResult = {
+          Reference: match[1],
+          Type: match[2],
+          SN: match[3],
+          Status: "SUKSES",
+          StartingBalance: match[4],
+          Amount: match[5],
+          EndingBalance: match[6],
+          TransactionType:  match[7]
+        };
+      }else if(loop == 4){
+        jsonResult = {
+          Reference: match[1],
+          Type: match[2],
+          StartingBalance: match[3],
+          Status: "GAGAL",
+          ProcessTime: match[4]
+        };
+      }
+
+      res.json(jsonResult);
+    } else {
+      if (text.includes("Nomor tujuan salah")) {
+        jsonResult = {
+          error: "Nomor tujuan salah"
+        };
+      }else if(text.includes("Saldo tidak cukup")){
+        jsonResult = {
+          error: "Saldo tidak cukup"
+        };
+      } else {
+        res.json({ error: text });
+      }
+      res.json({ error: "terjadi kesalahan" });
+    }
+  }catch(e){
+    res.json({ error: "terjadi kesalahan" });
+  }
+  
+})
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`apps running on port ${port}`);
